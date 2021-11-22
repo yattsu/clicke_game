d11075f0b16bc1a9bceaa92de8269719f97fce5f
unknown <email@domain>
2021-11-22 16:31:12 +0200

    solved savegame

--- scripts/game.js
+++ scripts/game.js
@@ -107,7 +107,6 @@ class Game {
   }
 
   saveGameObject = () => {
-    localStorage.setItem('gameObject', JSON.stringify(this));
     localStorage.setItem('score', JSON.stringify(this.score));
     localStorage.setItem('multipliers', JSON.stringify(this.multipliers));
     localStorage.setItem('settings', JSON.stringify(this.settings));
